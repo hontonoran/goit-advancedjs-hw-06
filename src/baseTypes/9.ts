44c0d5f61ef4c08dc86c8d4b413ed3d8dde369c5
hontonoran <hontonoran@gmail.com>
2026-06-21 16:17:08 +0300

    add typed Page objects

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -13,7 +13,7 @@ type Page = {
   };
 };
 
-const page1 = {
+const page1: Page = {
   title: "The awesome page",
   likes: 100,
   accounts: ["Max", "Anton", "Nikita"],
@@ -24,7 +24,7 @@ const page1 = {
   },
 };
 
-const page2 = {
+const page2: Page = {
   title: "Python or Js",
   likes: 5,
   accounts: ["Alex"],
